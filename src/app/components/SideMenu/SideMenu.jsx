595121f54b6f486ddc3c226ec8628c3f9e53fe55
manuelperezdearce <manuel.perezdearce@gmail.com>
2023-10-18 21:07:02 -0300

    working on sidemenu}

--- src/app/components/SideMenu/SideMenu.jsx
+++ src/app/components/SideMenu/SideMenu.jsx
@@ -1,14 +1,25 @@
-import { FaWhatsapp } from "react-icons/fa"
-
+"use client"
+import { FaLinkedin, FaWhatsapp } from "react-icons/fa"
+import { CgMenuGridR } from "react-icons/cg"
+import { useState } from "react"
 export default function SideMenu() {
-    return (
-        <div className="bg-customGrey rounded-lg p-3 text-4xl z-50 absolute bottom-[20%] left-[80vw] md:left-[95vw]  ">
-            <div className="flex justify-center items-center rounded-full box-border bg-green-600 hover:scale-110 hover:shadow-md duration-150 relative">
-                <div className="bg-green-600 rounded-full absolute h-[50px] w-[50px]">
 
-                </div>
-                <FaWhatsapp className="text-customWhite text-6xl absolute  " />
-            </div>
+    const [toggleMenu, setToggleMenu] = useState(false)
+
+    function handleToggleMenu() {
+        toggleMenu ? setToggleMenu(false) : setToggleMenu(true)
+    }
+    return (
+        <div className="rounded-full  text-4xl z-50 top-[calc(100vh-400px)] left-[calc(100vw-110px)] fixed flex flex-col gap-1"
+            onMouseEnter={handleToggleMenu}
+            onMouseLeave={handleToggleMenu}
+        >
+            <CgMenuGridR role="button" className="bg-customWhite rounded-full text-6xl p-1 text-primary z-50 hover:animate-spin"
+
+            />
+            <FaWhatsapp className={`bg-green-600 rounded-full text-customWhite text-6xl absolute transition duration-150 ease-in-out  ${toggleMenu ? "-top-16" : ""}`} />
+            <FaLinkedin className={`bg-customWhite rounded-full text-blue-600 text-6xl absolute ${toggleMenu ? "-top-32" : "top"}`} />
+
 
         </div>
     )
