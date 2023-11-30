097356133da33bd36efcd4c23dcba8d8e26287c4
manuelperezdearce <manuel.perezdearce@gmail.com>
2023-11-30 20:22:35 -0300

    Removed blur from ProjectCard

--- src/app/components/ProjectCard.jsx
+++ src/app/components/ProjectCard.jsx
@@ -5,7 +5,7 @@ export default function ProjectCard({ item }) {
     return (
         <article className="w-[300px] md:w-[740px] mx-auto text-sm sm:text-lg group flex flex-wrap justify-center gap-5 md:gap-10 font-light  ">
             <div className="rounded-3xl w-[320px] h-[188px] md:w-2/4 md:h-[250px]   bg-customFucsia2 overflow-hidden shadow-[0px_0px_50px_3px_rgba(255,255,255,0.1)]">
-                <img className="blur-[1px] bg-slate-500 brightness-90 w-full h-full rounded-3xl  sm:translate-x-9 sm:translate-y-9 group-hover:translate-x-0 group-hover:translate-y-0 group-hover:brightness-110 group-hover:blur-0 transition duration-500 shadow-[0px_0px_8px_3px_rgba(0,0,0,0.2)]" src={item.img}
+                <img className=" bg-slate-500 brightness-90 w-full h-full rounded-3xl  sm:translate-x-9 sm:translate-y-9 group-hover:translate-x-0 group-hover:translate-y-0 group-hover:brightness-110 group-hover:blur-0 transition duration-500 shadow-[0px_0px_8px_3px_rgba(0,0,0,0.2)]" src={item.img}
                     alt="imagen de proyecto" />
             </div>
             <div className="w-[300px] flex flex-col gap-2 md:gap-0 mx-auto text-left justify-between">
