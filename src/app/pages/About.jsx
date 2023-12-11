9e8eac101ff1929ae3b98d135f23c2692f12a35a
manuelperezdearce <manuel.perezdearce@gmail.com>
2023-12-11 16:47:11 -0300

    wot

--- src/app/pages/About.jsx
+++ src/app/pages/About.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-scroll"
 import ProfilePhoto from "../components/ProfilePhoto"
-import { Button } from "flowbite-react"
+import Button from "../components/Button"
+
 
 
 
@@ -14,49 +15,34 @@ export default function About({ styles }) {
             <span className="hidden xl:block">
                 <ProfilePhoto />
             </span>
-            <div className="w-full min-h-[800px] md:min-h-[950px] max-w-[400px] sm:max-w-[680px]   text-center z-40 flex flex-col items-center justify-center  md:px-0">
-                <div className="w-full  h-full z-40 flex flex-col items-center md:items-start justify-center relative" >
+            <div className="w-full min-h-[800px] md:min-h-[950px] md:max-w-[680px]   text-center z-40 flex flex-col items-center justify-center relative  md:px-0">
+                <div className="opacity-0 w-full  h-full z-40 flex flex-col items-center md:items-start justify-center relative" >
                     <h2 className="font-title font-black text-left                 bg-gradient-to-l from-customCalipso via-customCalips to-customFucsia
                     drop-shadow-md
-                    text-[35px] sm:text-5xl md:text-6xl lg:text-8xl mb-4 z-30 uppercase animate-fade-left animate-once animate-duration-1000 leading-tight sm:text-left whitespace-nowrap break-al  ">
-                        MANUel <br /> PÉREZ DE ARCE
+                    text-5xl sm:text-8xl lg:text-8xl mb-4 z-30 uppercase animate-fade-left animate-once animate-duration-1000 leading-tight sm:text-left whitespace-nowrap break-al  ">
+                        MANU.dev
                     </h2>
                     <h1 className="relative drop-shadow-md text-[rgba(23,45,66,1)] 
                     bg-gradient-to-r from-customCalipso via-customCalips to-customFucsia
-                    font-black sm:text-left text-2xl sm:text-4xl md:text-5xl z-30 animate-fade-left animate-once animate-duration-1000 uppercase sm:whitespace-nowrap">
+                    font-black sm:text-left text-2xl sm:text-5xl md:text-5xl z-30 animate-fade-left animate-once animate-duration-1000 uppercase sm:whitespace-nowrap">
                         Desarrollador Web
                         <span className="absolute -right-4 z-40 w-3 h-full bg-customCalipso animate-pulseText" />
                     </h1>
 
-                    <span className=" md:right-0 bottom-10 sm:bottom-0 absolute  block xl:hidden ">
-                        <ProfilePhoto />
-                    </span>
-                </div>
-                <p className="md:w-[680px] text-lg sm:text-3xl text-justif sm:text-center animate-fade-left animate-once animate-duration-250 animate-delay-500 mt-5 sm:mt-14">
-                    Hola, bienvenido a mi website!
-                    <br />
-                    Aquí podrás conocer un poco más de los trabajos que he realizado, además de las tecnologías que utilizo para crear sitios web asombrosos!
-                    <br />
 
-                    {/* ...o bien, si quieres contactarme <Link
-                        role="button"
-                        containerId="main"
-                        to="Contacto"
-                        smooth={true}
-                        className="text-customRed whitespace-nowrap"
-                    >pincha aquí.</Link> */}
+                </div>
+                <span className="z-0 md:-right-16 bottom-10 sm:bottom-[40%] absolute  block xl:hidden ">
+                    <ProfilePhoto />
+                </span>
+                <p className="hidden  sm:w-[560px] md:w-[680px] text-lg  font-medium sm:text-3xl sm:text-left animate-fade-left animate-once animate-duration-250 animate-delay-500 mt-5 sm:mt-14">
+                    ¡Bienvenido a mi sitio web! Aquí podrás conocer algunos de los proyectos en los que he trabajado, así como las tecnologías que utilizo para crear sitios web sorprendentes.
                 </p>
-                <Button className="mt-5 scale-125 rounded-full p-0 [&>span]:p-0 bg-secondary">
-                    <Link
-                        role="button"
-                        containerId="main"
-                        to="Contacto"
-                        smooth={true}
-                        className="whitespace-nowrap  px-4 py-2 rounded-full"
-                    >
-                        Contáctame
-                    </Link>
-                </Button>
+
+                <div className="flex justify-center items-center flex-wrap [&>a]:w-[40px] md:[&>a]:w-[80px] [&>a]:md:m-5 gap-2 [&>a]:hover:duration-200">
+                    <h3 className="text-3xl">Contáctame</h3>
+                    <a className="hover:scale-105 hover:shadow-lg rounded-full " target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/manuelperezdearce/"><img src="/redes/2.png" alt="" /></a>
+                    <a className="hover:scale-105" target="_blank" rel="noopener noreferrer" href="https://www.github.com/manuelperezdearce/"><img src="/redes/3.png" alt="" /></a>
+                </div>
 
 
 
