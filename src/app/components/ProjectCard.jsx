0506d186e5dda6f2a8aa1be38801301966772a49
manuelperezdearce <manuel.perezdearce@gmail.com>
2023-12-14 18:26:49 -0300

    nav to false

--- src/app/components/ProjectCard.jsx
+++ src/app/components/ProjectCard.jsx
@@ -7,8 +7,6 @@ export default function ProjectCard({ item }) {
             <div className="rounded-3xl w-[320px] h-[188px] md:w-2/4 md:h-[250px]   bg-customFucsia2 overflow-hidden shadow-[0px_0px_50px_3px_rgba(255,255,255,0.1)]">
                 <img className="bg-slate-500 brightness-90 w-full h-full rounded-3xl  sm:translate-x-9 sm:translate-y-9 group-hover:translate-x-0 group-hover:translate-y-0 group-hover:brightness-110 group-hover:blur-0 transition duration-500 shadow-[0px_0px_8px_3px_rgba(0,0,0,0.2)]" src={item.img}
                     alt="imagen de proyecto" />
-                {/* <Image width={400} height={300} className="bg-slate-500 brightness-90 w-full h-full rounded-3xl  sm:translate-x-9 sm:translate-y-9 group-hover:translate-x-0 group-hover:translate-y-0 group-hover:brightness-110 group-hover:blur-0 transition duration-500 shadow-[0px_0px_8px_3px_rgba(0,0,0,0.2)]" src={item.img}
-                    alt="imagen de proyecto" /> */}
             </div>
             <div className="w-[300px] flex flex-col gap-2 md:gap-0 mx-auto text-left justify-between">
                 <h3 className=" font-bold text-customFucsia2 text-2xl title-section group-hover:brightness-110 duration-150">{item.title}</h3>
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,32 +1,16 @@
-'use client'
+
 import Navbar from "./components/Navbar/Navbar";
 import Section from "./components/Section/Section";
 import About from "./pages/About";
 import Tecnologias from "./pages/Tecnologias";
 import Portafolio from "./pages/Portafolio";
-import { useDispatch } from 'react-redux'
-import { toggleATE } from "./features/slices/MainScrollSlice";
-
 import Experiencia from "./pages/Experiencia";
 import Home from "./pages/Home";
 import SectionFull from "./components/Section/SectionFull";
 
 export default function App() {
-  const dispatch = useDispatch()
-
-  const handleScroll = (e) => {
-    const scrollTop = e.target.scrollTop
-    const scrollHeight = e.target.scrollHeight
-    const offsetHeight = e.target.offsetHeight
-    if ((offsetHeight + scrollTop) >= scrollHeight) {
-      dispatch(toggleATE(true))
-    }
-    else
-      dispatch(toggleATE(false))
-  }
 
   return (
-
     <div name='CONTAINER' className="h-screen flex flex-col justify-top bg-customBlack10 relative">
       <Navbar />
       <main id="main" name="" className="w-screen overflow-x-hidden relative"
