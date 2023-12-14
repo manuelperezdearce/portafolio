82e296262ea738167f52093e4c8f259292e9c849
manuelperezdearce <manuel.perezdearce@gmail.com>
2023-12-14 18:49:05 -0300

    position navbar

--- next.config.js
+++ next.config.js
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'd33wubrfki0l68.cloudfront.net',
-                port: '',
-                pathname: '',
-            },
-        ],
-    },
+
 }
 
 module.exports = nextConfig
--- src/app/components/Navbar/components/NavUtilities.jsx
+++ src/app/components/Navbar/components/NavUtilities.jsx
@@ -12,7 +12,7 @@ export default function NavUtilities({ navItems, handleActiveIndex, activeIndex 
     return (
         <>
             <IoMdMenu className={`${toggleMenu && "active"} ms-auto text-4xl text-customCalipso hover:text-customFucsia  hover:scale-110 duration-150 hover:drop-shadow-[0px_0px_9px_rgba(255,255,255,0.5)]`} role='button' onClick={handleToggleMenu} />
-            <div className={`flex flex-col justify-between duration-200 bg-customBlack90 absolute top-[40px] md:top-[64px] w-2/3 -right-0 shadow-lg  md:w-1/3  ${!toggleMenu && "-right-full"} h-[calc(100vh-32px)] md:h-[calc(100vh-64px)] px-6 py-10 text-secondary`}
+            <div className={`flex flex-col justify-between duration-200 bg-customBlack90 absolute top-[36px] md:top-[64px] w-2/3 -right-0 shadow-lg  md:w-1/3  ${!toggleMenu && "-right-full"} h-[calc(100vh-32px)] md:h-[calc(100vh-64px)] px-6 py-10 text-secondary`}
                 onMouseLeave={handleToggleMenu}>
                 <ul
                 >
